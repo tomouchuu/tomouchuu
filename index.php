d868712263d77c65ed7539918de3f2ef52c7933e
Thomas Moore <tsanm93@gmail.com>
2015-08-11 20:53:53 +0100

    Update font-awesome version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>tomo＠パグ</title>
 
 	<link rel="stylesheet" href="bower_components/normalize.css/normalize.css">
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/2.4.1/octicons.css">
 	<link rel="stylesheet" href="assets/styles/app.css">
 </head>
