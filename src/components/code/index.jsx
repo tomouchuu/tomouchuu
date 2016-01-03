b7c51beacfa00fe1389da0703a829a0ff7de830a
Thomas Moore <tsanm93@gmail.com>
2016-01-03 22:03:26 +0000

    Added disbott project

--- src/components/code/index.jsx
+++ src/components/code/index.jsx
@@ -36,17 +36,23 @@ var Code = React.createClass({
 					</div>
 					<div className="projects-area">
 						<h2>Projects</h2>
-						<Project
-							ptitle="Doujin Release Tracker"
-							pimage="assets/images/projects/doujin-release-tracker.png"
-							pdesc="On it's 5th version, the doujin release tracker is built using Laravel with MongoDB for it's Database store. It uses Jquery Datatables heavily to allow users to filter and search for specific releases and runs atop a public API I created for it."
-							plink="http://doujinreleas.es"
+                        <Project
+							ptitle="Disbott"
+							pimage="assets/images/projects/disbott.png"
+							pdesc="Disbott is a bot for Discord. It's written in node and is primarily in use for some fun commands on mine and friends' discord servers."
+							plink="http://disbott.pagu.co"
 						/>
 						<Project
 							ptitle="Start (スタート)"
 							pimage="http://anoxy.se/uploads/2015/08/3jfgoc2h80r17la.png"
 							pdesc="This start page replaces your browser's new tab page and is a nice looking alternative which can be customisable with a few changes to the config file."
 							plink="https://github.com/tomopagu/start"
+						/>
+						<Project
+							ptitle="Doujin Release Tracker"
+							pimage="assets/images/projects/doujin-release-tracker.png"
+							pdesc="On it's 5th version, the doujin release tracker is built using Laravel with MongoDB for it's Database store. It uses Jquery Datatables heavily to allow users to filter and search for specific releases and runs atop a public API I created for it."
+							plink="http://doujinreleas.es"
 						/>
 					</div>
 				</div>
