0b6596ade3c40fc2051f1e77d1caf636acaa6f21
Thomas Moore <tomo@uchuu.io>
2017-04-30 19:44:57 +0100

    Add non-twitter oshis to the oshimen api

--- app/Http/Controllers/Api/OshimenController.php
+++ app/Http/Controllers/Api/OshimenController.php
@@ -10,11 +10,32 @@ use Twitter;
 class OshimenController extends Controller
 {
     public function get() {
+        $oshis = [];
+
+        $kanako = [
+            'name' => 'Kanako Momota',
+            'url' => 'http://ameblo.jp/momota-sd/',
+            'profile_image_url' => 'https://pbs.twimg.com/media/C-bAPqAVYAAD_Jy.jpg',
+            'screen_name' => 'momowgp',
+        ];
+        array_push($oshis, $kanako);
+
+        $mirei = [
+            'name' => 'Mirei Hoshina',
+            'url' => 'http://www.lineblog.me/tag/%E6%98%9F%E5%90%8D%E7%BE%8E%E6%80%9C?blog_name=ebichu',
+            'profile_image_url' => 'http://www.shiritsuebichu.jp/official/pc/img/profile/pro_photo_007.jpg',
+            'screen_name' => 'ebichu_staff',
+        ];
+        array_push($oshis, $mirei);
+
         $lists = Twitter::getLists();
         foreach ($lists as $list) {
             if ($list->slug === 'oshis') {
                 $listMembers = Twitter::getListMembers(['list_id' => $list->id]);
-                return response()->json($listMembers->users);
+                foreach ($listMembers->users as $member) {
+                    array_push($oshis, $member);
+                }
+                return response()->json($oshis);
             }
         }
     }
