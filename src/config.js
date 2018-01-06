3cef8522a06b536e962ca3d0580636606cad8f42
Thomas Moore <tomo@uchuu.io>
2018-01-06 11:25:45 +0000

    Point to live api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
-  api: 'http://localhost:8080',
-  // api: 'https://api-tomo.uchuu.io',
+  // api: 'http://localhost:8080',
+  api: 'https://api-tomo.uchuu.io',
   github: 'https://github.com/tomouchuu/tomo.uchuu.io',
   twitter: 'https://twitter.com/tomouchuu'
 };
