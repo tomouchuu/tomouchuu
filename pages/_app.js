d2882d46d57446964dfa323594914f2dbabc9dc3
Thomas Moore <tomo@uchuu.io>
2021-01-16 14:48:33 +0000

    Switch to modern base App component + SEO improv

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,21 @@
 import React from 'react';
-
-import App from 'next/app';
 import Head from 'next/head';
 
-// import "tailwindcss/tailwind.css";
 import '../styles/globals.css';
 
-export default class MyApp extends App {
-    render() {
-        const {Component, pageProps} = this.props
-        return (
-            <>
-                <Head>
-                    <meta charSet="utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                </Head>
-                <Component {...pageProps} />
-            </>
-        )
-    }
-}
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <html lang="en" />
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="Description" content="Personal/Portfolio site for Thomas Moore. Frontend Developer from Chelmsford, UK"></meta>
+                <title>Thomas Moore</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+};
+
+export default MyApp;
