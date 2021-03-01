a11b0654f1b65860a29fe16940178c9c6ece42af
Thomas Moore <tomo@uchuu.io>
2021-03-01 20:23:42 +0000

    Fix random next-head-count issue

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <html lang="en" />
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                 <meta name="description" content="Personal/Portfolio site for Thomas Moore. Frontend Developer from Chelmsford, UK" />
