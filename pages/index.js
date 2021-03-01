8e83f1ff9d6266c19b54df1397eb3466a8898a65
Thomas Moore <tomo@uchuu.io>
2021-03-01 19:55:24 +0000

    Switch to the new graphql tomo-api

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import useSWR from 'swr';
+import { request, gql } from 'graphql-request';
+
 import Link from 'next/link';
 
 import employmentMarkup from '../utils/employment-text';
@@ -10,46 +12,103 @@ import CurrentMusic from '../components/CurrentMusic';
 import CurrentTweet from '../components/CurrentTweet';
 import LastGithub from '../components/LastGithub';
 
-const fetcher = url => fetch(url).then(r => r.json());
+const fetcher = query => request('https://api-tomo.uchuu.io/api/me', query)
+
+const query = gql`{
+	personal {
+        name
+        birthday
+        location
+        based
+        contact {
+            email
+            twitter
+            github
+            applemusic
+            lastfm
+            discord
+            youtube
+            twitch
+            linkedin
+        }
+        work {
+            company
+        }
+    }
+    music {
+        album {
+            name
+            url
+        }
+        artist {
+            name
+            url
+        }
+        track {
+            name
+            url
+            playedCount
+        }
+    }
+    github(limit: 1) {
+        payload {
+            action
+            issue {
+                number
+            }
+            ref
+            ref_type
+        }
+        repo {
+            name
+        }
+        type
+    }
+    twitter {
+        user {
+            profile_image_url_https
+            status {
+                created_at
+                id_str
+                full_text
+            }
+        }
+    }
+}`;
 
 export async function getStaticProps() {
-    const baseData = await fetcher('https://api-tomo.uchuu.io/');
-    const twitterData = await fetcher('https://api-tomo.uchuu.io/twitter/user.js');
-    const githubData = await fetcher('https://api-tomo.uchuu.io/github');
-    const lastfmData = await fetcher('https://api-tomo.uchuu.io/lastfm');
+    const data = await fetcher(query);
 
     return {
         props: {
-            baseData, githubData, twitterData, lastfmData
+            data
         },
         revalidate: 21600 //6hours
     };
 };
 
 const Home = (props) => {
-    const { data } = useSWR('https://api-tomo.uchuu.io/', fetcher, { initialData: props.baseData });
-    const { data: twitterData } = useSWR('https://api-tomo.uchuu.io/twitter/user.js', fetcher, { initialData: props.twitterData });
-    const { data: githubData } = useSWR('https://api-tomo.uchuu.io/github', fetcher, { initialData: props.githubData });
-    const { data: lastfmData } = useSWR('https://api-tomo.uchuu.io/lastfm', fetcher, {initialData: props.lastfmData});
+    const { data } = useSWR(query, fetcher, { initialData: props.data });
+    const {personal, github: lastGithub, music, twitter: {user: twitterData}} = data;
     
-    const basedInText = data.me.based ? ` and working in <b>${data.me.based}</b>` : '';
+    const basedInText = personal.based ? ` and working in <b>${personal.based}</b>` : '';
 
     return (
         <Base>
             <div>
                 <Intro
                     based={basedInText}
-                    birthday={ageText(data.me.birthday)}
-                    contact={data.me.contact}
-                    employment={employmentMarkup(data.me)}
-                    image={twitterData?.profile_image_url_https.replace('_normal', '')}
-                    location={data.me.location}
-                    name={data.me.name}
+                    birthday={ageText(personal.birthday)}
+                    contact={personal.contact}
+                    employment={employmentMarkup(personal)}
+                    image={twitterData.profile_image_url_https}
+                    location={personal.location}
+                    name={personal.name}
                 />
                 <div className="px-4 mb-8 md:px-2">
-                    <CurrentMusic music={lastfmData} />
+                    <CurrentMusic music={music} />
                     <CurrentTweet tweet={twitterData} />
-                    <LastGithub data={githubData[0]} />
+                    <LastGithub data={lastGithub[0]} />
                     <p className="mt-4 text-2xl">
                         <Link href="/me"><a>More about me</a></Link>
                     </p>
--- pages/me.js
+++ pages/me.js
@@ -1,32 +1,46 @@
+import { request, gql } from 'graphql-request';
+
 import Link from 'next/link';
-import fetch from 'isomorphic-unfetch';
 
 import {employmentText} from '../utils/employment-text';
 import ageText from '../utils/age-text';
 
-export async function getStaticProps() {
-    const base = await fetch('https://api-tomo.uchuu.io/');
-    const baseData = await base.json();
+const fetcher = query => request('https://api-tomo.uchuu.io/api/me', query)
 
-    const twitter = await fetch('https://api-tomo.uchuu.io/twitter/user.js');
-    const twitterData = await twitter.json();
+export async function getStaticProps() {
+    const data = await fetcher(gql`{
+        personal {
+            name
+            birthday
+            location
+            based
+            work {
+                company
+            }
+        }
+        twitter {
+            user {
+                profile_image_url_https
+            }
+        }
+    }`);
 
     return {
         props: {
-            baseData, twitterData
+            data
         },
         revalidate: 21600 //6hours
     };
 }
 
 function Me(props) {
-    const {baseData: data, twitterData} = props;
+    const {personal, twitter: {user: twitterData}} = props.data;
 
     return (
         <div className="container max-w-screen-md mx-auto text-lg text-justify mt-4 p-4 md:mt-12 md:p-0">
             <Link href="/"><a className="text-2xl">Back</a></Link>
-            <img src={twitterData.profile_image_url_https.replace('_normal', '')} alt={data.me.name} className="block rounded-full mx-auto my-8 w-72" />
-            <p>So hello there, I'm {data.me.name}. I'm {ageText(data.me.birthday)}, from {data.me.location} and currently {employmentText(data.me)}.</p>
+            <img src={twitterData.profile_image_url_https} alt={personal.name} className="block rounded-full mx-auto my-8 w-72" />
+            <p>So hello there, I'm {personal.name}. I'm {ageText(personal.birthday)}, from {personal.location} and currently {employmentText(personal)}.</p>
             <p className="my-8">I'm a frontend developer with a focus on building components with javascript frameworks like reactjs & vuejs along with knowledge of a combination of most css solutions (css, scss, less, stylus, postcss, css in js). Storybook is ❤. I do also a little bit of backend work, either in javascript with node or php where I use laravel.</p>
             <p className="my-8">Outside of coding, I'm a big fan of things japanese but mostly enjoy music (and mostly IDOL at that) and aesthetics. I try to get over there every 2 years at least and I'm trying to learn the language via a tutor, self teaching and going to meetups.</p>
             <p className="my-8">I also play drums occasionally, used to be in a band but I still keep playing to keep skills up, because of that I do finger drum in the office (sorry not sorry).</p>
