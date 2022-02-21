af6ad476dcb14e078518b9322ebffb2ff21cc5b4
Thomas Moore <tomo@uchuu.io>
2022-02-21 20:25:51 +0000

    Make profile image circular again

--- components/Intro.js
+++ components/Intro.js
@@ -17,7 +17,9 @@ const Intro = props => {
     return (
         <div className="container max-w-screen-md mx-auto bg-black bg-opacity-60 mb-8 sm:border sm:border-gray-50 sm:rounded text-3xl p-8 shadow-xl z-10">
             <div className="flex flex-wrap align-middle justify-around">
-                <Image className="rounded-full mr-8 w-52 h-52 !border-2 border-gray-50 flex-none" src={ProfilePicture} alt={name} width="206" height="206" placeholder="blur" />
+                <div className='mr-2'>
+                    <Image className="rounded-full w-52 h-52 !border-2 border-gray-50 flex-none" src={ProfilePicture} alt={name} width="208" height="208" placeholder="blur" />
+                </div>
                 <div className="flex-1">
                     Hi I'm <h1 className="inline font-bold text-5xl">{name}</h1>, <b className="text-4xl">{birthday} years old</b>, from <b className="text-4xl">{location}</b><span dangerouslySetInnerHTML={{__html: based}} /> where I'm currently <span dangerouslySetInnerHTML={employment} />.
                 </div>
