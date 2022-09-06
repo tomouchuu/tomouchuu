83242a0092aea53ed9f3a7704f859c5e74f4631e
Thomas Moore <tomo@uchuu.io>
2022-09-06 22:17:18 +0100

    More whitespace + text update

--- utils/age-text.js
+++ utils/age-text.js
@@ -1,9 +1,9 @@
 import {differenceInYears, parse} from 'date-fns';
 
 const ageText = (birthday) => {
-    const now = new Date();
-    const parsedBirthday = parse(birthday, 'yyyy-MM-dd', now);
-    return differenceInYears(now, parsedBirthday);
+  const now = new Date();
+  const parsedBirthday = parse(birthday, 'yyyy-MM-dd', now);
+  return differenceInYears(now, parsedBirthday);
 }
 
 export default ageText;
