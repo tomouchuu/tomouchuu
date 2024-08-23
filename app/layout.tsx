37fe0640c33ac70306b995a1eaa73f803363716b
Thomas Moore <thomas.moore@hey.com>
2024-08-23 17:15:55 +0100

    Add next-themes for colour scheme work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider"
 import { Separator } from "@/components/ui/separator"
 
 export const metadata: Metadata = {
@@ -14,15 +15,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-          {children}
-          <footer className="text-center text-xs min-w-36">
-            <Separator />
-            <p className="mt-4">トー マス＠宇宙</p>
-          </footer>
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
+            {children}
+            <footer className="text-center text-xs min-w-36">
+              <Separator />
+              <p className="mt-4">トー マス＠宇宙</p>
+            </footer>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
