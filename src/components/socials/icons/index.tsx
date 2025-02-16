b5396a235c3aba0d47f23204f09dd2518c02c73e
Thomas Moore <thomas.moore@hey.com>
2025-02-16 15:15:19 +0000

    Adds social icon svgs

---
+++ src/components/socials/icons/index.tsx
@@ -0,0 +1,6 @@
+export { Discord } from "./discord";
+export { Github } from "./github";
+export { Instagram } from "./instagram";
+export { Twitch } from "./twitch";
+export { Twitter } from "./twitter";
+export { Youtube } from "./youtube";
