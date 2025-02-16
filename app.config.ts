6477667dd70eb60e2539b76f5121ffa45a350176
Thomas Moore <thomas.moore@hey.com>
2025-02-16 17:12:38 +0000

    Update the app config for sst to work

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    preset: "aws-lambda",
+    awsLambda: {
+      streaming: true,
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
