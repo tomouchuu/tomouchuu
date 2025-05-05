5dca936e4973402f3d2612cb9982387033af1b27
Thomas Moore <thomas.moore@hey.com>
2025-05-05 21:31:54 +0100

    Update the about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -29,23 +29,23 @@ export default function About() {
         <p>So hello there, I'm Thomas Moore.</p>
         <p class="my-4">
           I'm a UI engineer with experience in React with frameworks like
-          Next.js, Remix(RR) and Astro. Right now I'm playing around with Solid
-          and I'm a big fan of TailwindCSS
+          Next.js, Remix(RR) and Tanstack. Right now I'm playing around with
+          Solid, Cloudflare resources and I'm a big fan of TailwindCSS.
         </p>
         <p class="my-4">
           Outside of coding, I'm a big fan of things japanese but mostly enjoy
           music (and mostly IDOL at that) and aesthetics. I try to get over
-          there every year at least and I'm trying to learn the language via a
-          tutor, self teaching and going to meetups.
+          there a few times a year at least, but my attempts to learn the
+          language are slow going. Mainly because I rely on machine translations
+          (I built my own!) and bad sites like duolingo.
         </p>
         <p class="my-4">
-          I play the critically acclaimed MMORPG Final Fantasy 14 to a
+          I played the critically acclaimed MMORPG Final Fantasy 14 to a
           mid-to-high level (in my opinion). I tend to play support jobs such as
           Bard or Dancer to help my team mates increase damage to clear fights
-          (It also has a pretty good story too I guess). Main achievement there
-          is clearing an ultimate on the same expansion which took over 1150
-          attempts over a 2 month time period! You can find out more about my
-          character{" "}
+          (It also has a pretty good story too I guess). Main achievements there
+          include clearing an ultimate on the same patch! You can find out more
+          about my character{" "}
           <a
             class="underline"
             href="https://tomestone.gg/character/16899179/tomo-uchuu"
