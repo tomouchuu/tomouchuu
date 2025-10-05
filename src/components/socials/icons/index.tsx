f45a782fb652117062780956f9182e1e9250d263
Thomas Moore <thomas.moore@hey.com>
2025-10-05 16:54:26 +0100

    Remove the src folder

--- src/components/socials/icons/index.tsx
+++
@@ -1,6 +0,0 @@
-export { Discord } from "./discord";
-export { Github } from "./github";
-export { Instagram } from "./instagram";
-export { Twitch } from "./twitch";
-export { Twitter } from "./twitter";
-export { Youtube } from "./youtube";
