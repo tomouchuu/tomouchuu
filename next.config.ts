3f67aa847d21c04f23d37078b7be1cbf00c5fe03
Thomas Moore <thomas.moore@hey.com>
2025-10-05 18:22:53 +0100

    Update to canary

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: "incremental",
+  },
 };
 const withNextIntl = createNextIntlPlugin();
 
