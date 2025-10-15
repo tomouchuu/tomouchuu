66b9b02641fd444b4ad27e11bc4f741fb693d40e
Thomas Moore <thomas.moore@hey.com>
2025-10-16 00:07:18 +0100

    Add working lastfm integration
    To update design and such but it's at least getting all the data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,9 @@
   import { Separator } from "$lib/components/ui/separator/index.js";
   import { m } from '$lib/paraglide/messages.js';
 
+  import { QueryClientProvider } from '@tanstack/svelte-query'
+  import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools'
+
   import { dev } from '$app/environment';
   import { injectAnalytics } from '@vercel/analytics/sveltekit';
   import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
@@ -12,20 +15,24 @@
   injectAnalytics({ mode: dev ? 'development' : 'production' });
   injectSpeedInsights();
 
-	let { children } = $props();
+	const { data, children } = $props()
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<div class="flex min-h-screen flex-col items-center justify-center gap-4">
-  {@render children?.()}
-  <footer class="text-center text-xs min-w-36">
-    <Separator />
-    <p class="mt-4">
-      {m.thomas()}@<a href="https://uchuu.dev" class="hover:underline">{m.uchuu()}</a>
-    </p>
-  </footer>
-</div>
+<QueryClientProvider client={data.queryClient}>
+  <div class="flex min-h-screen flex-col items-center justify-center gap-4">
+    {@render children?.()}
+    <footer class="text-center text-xs min-w-36">
+      <Separator />
+      <p class="mt-4">
+        {m.thomas()}@<a href="https://uchuu.dev" class="hover:underline">{m.uchuu()}</a>
+      </p>
+    </footer>
+  </div>
+  <SvelteQueryDevtools />
+</QueryClientProvider>
+
 
