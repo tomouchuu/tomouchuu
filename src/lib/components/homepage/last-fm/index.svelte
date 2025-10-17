b98cd47c928f67ebdd64127282353d65b0ec24a7
Thomas Moore <thomas.moore@hey.com>
2025-10-17 20:20:51 +0100

    Lastfm component complete

--- src/lib/components/homepage/last-fm/index.svelte
+++ src/lib/components/homepage/last-fm/index.svelte
@@ -2,6 +2,9 @@
   import { createQuery } from '@tanstack/svelte-query'
   import { fetchLastfmData, type LastfmResult } from './index.js'
 
+  import { Skeleton } from '$lib/components/ui/skeleton/index.js';
+  import HeadphonesIcon from '@lucide/svelte/icons/headphones';
+
   const lastfm = createQuery<
     LastfmResult,
     Error
@@ -12,17 +15,63 @@
     staleTime: 30000, // 30s
     retry: 1,
   }))
+
+  function getOrdinal(n: number) {
+    let ord = "th";
+
+    if (n % 10 == 1 && n % 100 != 11) ord = "st";
+    else if (n % 10 == 2 && n % 100 != 12) ord = "nd";
+    else if (n % 10 == 3 && n % 100 != 13) ord = "rd";
+
+    return ord;
+  }
+
+  let trackPlayed = $derived(lastfm.data?.track?.playedCount === 0 ? 1 : (lastfm.data?.track?.playedCount ?? 1));
 </script>
 
-{#if lastfm.status === 'pending'}
-  <span>Loading...</span>
-{:else if lastfm.status === 'error'}
-  <span>Error: {lastfm.error?.message}</span>
-{:else}
-  <span>Album: {lastfm.data?.album?.name}</span>
-  <span>Artist: {lastfm.data?.artist?.name}</span>
-  <span>Track: {lastfm.data?.track?.name}</span>
-  {#if lastfm.data?.isLive}
-    <span>(Live)</span>
-  {/if}
-{/if}
+<div class="flex justify-center items-center my-2 text-lg w-full">
+  <HeadphonesIcon class="flex-none mr-4" />
+  {#if lastfm.status === 'pending'}
+    <Skeleton class="h-8 rounded-lg flex-grow" style="height: ''" />
+  {:else if lastfm.status === 'error'}
+    <p>Could not load lastfm data</p>
+  {:else}
+    <p>
+      <a
+        href={lastfm.data.track?.url}
+        title={lastfm.data.track?.name}
+        target="_blank"
+        rel="noopener"
+        class="hover:underline"
+      >
+        {lastfm.data.track?.name}
+      </a>
+      {" by "}
+      <a
+        href={lastfm.data.artist?.url}
+        title={lastfm.data.artist?.name}
+        target="_blank"
+        rel="noopener"
+        class="hover:underline"
+      >
+        {lastfm.data.artist?.name}
+      </a>
+      {" from "}
+      <a
+        href={lastfm.data.album?.url}
+        title={lastfm.data.album?.name}
+        target="_blank"
+        rel="noopener"
+        class="hover:underline"
+      >
+        {lastfm.data.album?.name}
+      </a>
+      {` for the `}
+      <span class="ordinal">
+        {trackPlayed}{getOrdinal(trackPlayed)}
+      </span>
+      {` time`}
+       {lastfm.data.isLive && " right now!"}
+     </p>
+   {/if}
+ </div>
