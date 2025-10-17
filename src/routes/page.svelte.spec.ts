c86810748f65da26747a85e68f12285cc30a907a
Thomas Moore <thomas.moore@hey.com>
2025-10-17 22:25:48 +0100

    Get tests passing again

--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -1,11 +1,11 @@
 import { page } from "@vitest/browser/context";
 import { describe, expect, it } from "vitest";
 import { render } from "vitest-browser-svelte";
-import Page from "./+page.svelte";
+import PageTestWrapper from "../test/PageTestWrapper.svelte";
 
 describe("/+page.svelte", () => {
   it("should render h1", async () => {
-    render(Page);
+    render(PageTestWrapper);
 
     const heading = page.getByRole("heading", { level: 1 });
     await expect.element(heading).toBeInTheDocument();
