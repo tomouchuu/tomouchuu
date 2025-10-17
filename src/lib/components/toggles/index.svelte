18586119fb285434e940c22a5593cc569ab03654
Thomas Moore <thomas.moore@hey.com>
2025-10-17 20:59:04 +0100

    Add toggles and styling to layout

---
+++ src/lib/components/toggles/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import * as ButtonGroup from "$lib/components/ui/button-group/index.js";
+  import ThemeToggle from "$lib/components/toggles/theme-toggle.svelte";
+  import LanguageToggle from "$lib/components/toggles/language-toggle.svelte";
+</script>
+
+<ButtonGroup.Root
+  orientation="vertical"
+  aria-label="Toggles"
+  class="fixed top-4 right-5 cursor-pointer h-fit"
+>
+  <ThemeToggle />
+  <LanguageToggle />
+</ButtonGroup.Root>
