13984711de376f6e440f047c8d2f347ebb07039a
Thomas Moore <thomas.moore@hey.com>
2025-10-18 17:40:39 +0100

    Look at ISR and prerendering the about page

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,9 +1,16 @@
+import type { Config } from '@sveltejs/adapter-vercel';
 import type { PageLoad } from './$types'
 import type { App } from "./api/[...slugs]/+server";
 
 import { treaty } from "@elysiajs/eden"
 import { fetchLastfmData } from '$lib/components/homepage/last-fm/index.js'
 import { getBaseUrl } from "$lib/utils";
+
+export const config: Config = {
+  isr: {
+    expiration: 120,
+  },
+};
 
 export const load: PageLoad = async ({ parent }) => {
   const { queryClient } = await parent()
