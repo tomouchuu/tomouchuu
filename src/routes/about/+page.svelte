7850c2577cb34d33ab809ff75c0668728851e469
Thomas Moore <thomas.moore@hey.com>
2025-11-02 14:52:16 +0000

    quote the bad language learning site

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -32,7 +32,7 @@
        I'm a UI engineer with experience in React with frameworks like Next.js and Tanstack. With Ember experience for the day job. Though right now I'm playing around with Svelte, Solid, Cloudflare resources and I'm a big fan of TailwindCSS.
      </p>
      <p class="my-4">
-       Outside of coding, I'm a big fan of things japanese but mostly enjoy music (and mostly IDOL at that) and aesthetics. I try to get over there a few times a year at least, but my attempts to learn the language are slow going. Mainly because I rely on machine translations (I built my own app, though currently I use Kagi) and bad sites like duolingo.
+       Outside of coding, I'm a big fan of things japanese but mostly enjoy music (and mostly IDOL at that) and aesthetics. I try to get over there a few times a year at least, but my attempts to learn the language are slow going. Mainly because I rely on machine translations (I built my own app, though currently I use Kagi) and "bad" sites like duolingo.
      </p>
      <p class="my-4">
        I played the critically acclaimed MMORPG Final Fantasy 14 to a mid-to-high level (in my opinion). I tend to play support jobs such as Bard or Dancer to help my team mates increase damage to clear fights (It also has a pretty good story too I guess). Main achievements there include clearing an ultimate on the same patch! You can find out more about my character <a class="underline" href="https://tomestone.gg/character/16899179/tomo-uchuu" title="Tomo's FF14 character">here</a>.
